ea1f8905382c9585991c0eb92bb65c9463260298
Bournane Mohamed <mbournane@gmail.com>
2023-06-26 09:48:09 +0200

    test git

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System;
+
+
+namespace StudentsGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
